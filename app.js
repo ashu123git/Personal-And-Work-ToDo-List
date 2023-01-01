4f2ba2786834da18c7e0a37106b133597d4877f0
ashu24998 <ashutosh24998@gmail.com>
2023-01-01 11:07:49 +0530

    Created new Project

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("Public"));
+app.use(bodyParser.urlencoded({extended:true}));
+const personalToDo = [];
+const workToDo = [];
+
+app.get("/", function(req, res) {
+    const myDate = new Date();
+    var options = {
+        weekday: 'long',
+        month: 'long', 
+        day: 'numeric'
+    }
+
+    const dayName = myDate.toLocaleDateString("en-US", options);
+    res.render("data", {
+        dayShow: dayName,
+        personalTD: personalToDo
+    });
+})
+
+app.get("/work", function(req, res) {
+    res.render("data", {
+        dayShow: "Work List",
+        personalTD: workToDo
+    })
+})
+
+app.post("/", function(req, res) {
+    var itemName = req.body.text1;
+    var todoName = req.body.submit;
+    if(todoName === "Work") {
+        workToDo.push(itemName);
+        res.redirect("/work");
+    }
+    else {
+        personalToDo.push(itemName);
+        res.redirect("/");
+    }
+})
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000. Press Ctrl+C to quit server...")
+})
